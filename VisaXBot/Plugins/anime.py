21e2a04894a7078c716871ee3c83057d8c3eaad0
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-09-08 22:18:50 +0530

    Update anime.py

--- VisaXBot/Plugins/anime.py
+++ VisaXBot/Plugins/anime.py
@@ -67,7 +67,7 @@ async def formatJSON(outData):
         return msg
 
 
-@bot.on(deadly_cmd(pattern="anilist (.*)"))
+@bot.on(visa_cmd(pattern="anilist (.*)"))
 @bot.on(sudo_cmd(pattern="anilist (.*)", allow_sudo=True))
 async def anilist(event):
     if event.fwd_from:
@@ -79,11 +79,11 @@ async def anilist(event):
     await event.edit(msg, link_preview=True)
 
 
-@bot.on(deadly_cmd(pattern="anime(?: |$)(.*)"))
+@bot.on(visa_cmd(pattern="anime(?: |$)(.*)"))
 @bot.on(sudo_cmd(pattern="anime(?: |$)(.*)", allow_sudo=True))
 async def nope(hel_):
-    deadly = hel_.pattern_match.group(1)
-    if not deadly:
+    visa = hel_.pattern_match.group(1)
+    if not visa:
         if hel_.is_reply:
             (await hel_.get_reply_message()).message
         else:
@@ -91,7 +91,7 @@ async def nope(hel_):
             )
             return
 
-    troll = await bot.inline_query("AniFluidbot", f".anime {(deEmojify(deadly))}")
+    troll = await bot.inline_query("AniFluidbot", f".anime {(deEmojify(vis))}")
 
     await troll[0].click(
         hel_.chat_id,
@@ -102,11 +102,11 @@ async def nope(hel_):
     await hel_.delete()
     
     
-@bot.on(deadly_cmd(pattern="manga(?: |$)(.*)"))
+@bot.on(visa_cmd(pattern="manga(?: |$)(.*)"))
 @bot.on(sudo_cmd(pattern="manga(?: |$)(.*)", allow_sudo=True))
 async def nope(hel_):
-    deadly = hel_.pattern_match.group(1)
-    if not deadly:
+    visa = hel_.pattern_match.group(1)
+    if not visa:
         if hel_.is_reply:
             (await hel_.get_reply_message()).message
         else:
@@ -114,7 +114,7 @@ async def nope(hel_):
             )
             return
 
-    troll = await bot.inline_query("AniFluidbot", f".manga {(deEmojify(deadly))}")
+    troll = await bot.inline_query("AniFluidbot", f".manga {(deEmojify(visa))}")
 
     await troll[0].click(
         hel_.chat_id,
@@ -125,11 +125,11 @@ async def nope(hel_):
     await hel_.delete()
     
 
-@bot.on(deadly_cmd(pattern="character(?: |$)(.*)"))
+@bot.on(visa_cmd(pattern="character(?: |$)(.*)"))
 @bot.on(sudo_cmd(pattern="character(?: |$)(.*)", allow_sudo=True))
 async def nope(hel_):
-    deadly = hel_.pattern_match.group(1)
-    if not deadly:
+    visa = hel_.pattern_match.group(1)
+    if not visa:
         if hel_.is_reply:
             (await hel_.get_reply_message()).message
         else:
@@ -137,7 +137,7 @@ async def nope(hel_):
             )
             return
 
-    troll = await bot.inline_query("AniFluidbot", f".character {(deEmojify(deadly))}")
+    troll = await bot.inline_query("AniFluidbot", f".character {(deEmojify(visa))}")
 
     await troll[0].click(
         hel_.chat_id,
