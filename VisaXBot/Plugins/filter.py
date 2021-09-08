70a65979579d1b72fa7c888fb402d207b85359f0
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-09-08 22:57:35 +0530

    Update filter.py

--- VisaXBot/Plugins/filter.py
+++ VisaXBot/Plugins/filter.py
@@ -15,7 +15,7 @@ TYPE_DOCUMENT = 2
 global last_triggered_filters
 last_triggered_filters = {}  # pylint:disable=E0602
 
-@bot.on(deadly_cmd(incoming=True))
+@bot.on(visa_cmd(incoming=True))
 async def on_snip(event):
     global last_triggered_filters
     name = event.raw_text
@@ -57,7 +57,7 @@ async def on_snip(event):
                 last_triggered_filters[event.chat_id].remove(name)
 
 
-@bot.on(deadly_cmd(pattern=r"filter (.*)"))
+@bot.on(visa_cmd(pattern=r"filter (.*)"))
 @bot.on(sudo_cmd(pattern=r"filter (.*)", allow_sudo=True))
 async def on_snip_save(event):
     if event.fwd_from:
@@ -84,7 +84,7 @@ async def on_snip_save(event):
         await eod(event, f"Reply to a message with `{hl}filter keyword` to save the filter")
 
 
-@bot.on(deadly_cmd(pattern="filters$"))
+@bot.on(visa_cmd(pattern="filters$"))
 @bot.on(sudo_cmd(pattern="filters$", allow_sudo=True))
 async def on_snip_list(event):
     if event.fwd_from:
@@ -112,7 +112,7 @@ async def on_snip_list(event):
         await eod(event, OUT_STR)
 
 
-@bot.on(deadly_cmd(pattern="stop (.*)"))
+@bot.on(visa_cmd(pattern="stop (.*)"))
 @bot.on(sudo_cmd(pattern="stop (.*)", allow_sudo=True))
 async def on_snip_delete(event):
     if event.fwd_from:
@@ -122,7 +122,7 @@ async def on_snip_delete(event):
     await eod(event, f"Filter `{name}` deleted successfully")
 
 
-@bot.on(deadly_cmd(pattern="rmallfilters$"))
+@bot.on(visa_cmd(pattern="rmallfilters$"))
 @bot.on(sudo_cmd(pattern="rmallfilters$", allow_sudo=True))
 async def on_all_snip_delete(event):
     if event.fwd_from:
