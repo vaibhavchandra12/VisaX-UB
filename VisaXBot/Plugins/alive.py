4934e85fc5d766ebf201985fcf7e62a3acd8fbdb
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-09-08 21:48:13 +0530

    Update alive.py

--- VisaXBot/Plugins/alive.py
+++ VisaXBot/Plugins/alive.py
@@ -13,54 +13,54 @@ from . import *
 #-------------------------------------------------------------------------------
 DEFAULTER = Config.YOUR_NAME
 
-@bot.on(deadly_cmd(outgoing=True, pattern="alive$"))
+@bot.on(visa_cmd(outgoing=True, pattern="alive$"))
 @bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
-async def up(deadly):
-    if deadly.fwd_from:
+async def up(visa):
+    if visa.fwd_from:
         return
-    await deadly.get_chat()
-    await deadly.delete()
-    await bot.send_file(deadly.chat_id, DEADLY_PIC, caption=DEADLY_CAPTION)
-    await deadly.delete()
+    await visa.get_chat()
+    await visa.delete()
+    await bot.send_file(visa.chat_id, VISA_PIC, caption=VISA_CAPTION)
+    await visa.delete()
 
-DEADLY_PIC = Config.ALIVE_PIC or "https://telegra.ph/file/37ac22fe95355d62c2d76.mp4"
-DEADLY_CAPTION = "🔥 ℓєgєη∂яу αƒ ∂єα∂ℓу кααℓ 🔥\n\n"
-DEADLY_CAPTION += (
-    f"                __↼𝙼𝙰𝚂𝚃𝙴𝚁⇀__\n  **『 {deadly_mention} 』**\n\n"
+VISA_PIC = Config.ALIVE_PIC or "https://telegra.ph/file/37ac22fe95355d62c2d76.mp4"
+VISA_CAPTION = "🔥 ʟɛɢɛռɖǟʀʏ ǟʄ ʋɨֆǟ-Ӽ ɮօȶ 🔥\n\n"
+VISA_CAPTION += (
+    f"                __↼𝙼𝙰𝚂𝚃𝙴𝚁⇀__\n  **『 {visa_mention} 』**\n\n"
 )
-DEADLY_CAPTION += f"╔═══════════════╗\n"
-DEADLY_CAPTION += f"╠•➳➠ `𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽:` `{tel_ver}` \n"
-DEADLY_CAPTION += f"╠•➳➠ `𝚅𝙴𝚁𝚂𝙸𝙾𝙽:` `{deadly_ver}`\n"
-DEADLY_CAPTION += f"╠•➳➠ `𝙶𝚁𝙾𝚄𝙿:`  [𝙹𝙾𝙸𝙽](t.me/DEADLY_KAAL_SUPPORT)\n"
-DEADLY_CAPTION += f"╠•➳➠ `𝙲𝙷𝙰𝙽𝙽𝙴𝙻:` [𝙹𝙾𝙸𝙽](t.me/deadly_kaal_bot)\n"
-DEADLY_CAPTION += f"╠•➳➠ `𝙲𝚁𝙴𝙰𝚃𝙾𝚁:` [⚡𝙿𝚁𝙾⚡](https://t.me/DEADLY_TECHY/83)\n"
-DEADLY_CAPTION += f"╚═══════════════╝\n\n"
-DEADLY_CAPTION += " [✨𝚁𝙴𝙿𝙾✨](https://github.com/DEADLY-FIGHTERS/DEADLY-KAAL-BOT) 🔹 [📜𝙻𝙸𝙲𝙴𝙽𝚂𝙴📜](https://github.com/DEADLY-FIGHTERS/DEADLY-KAAL-BOT/blob/main/LICENSE)"
+VISA_CAPTION += f"╔═══════════════╗\n"
+VISA_CAPTION += f"╠•➳➠ `𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽:` `{tel_ver}` \n"
+VISA_CAPTION += f"╠•➳➠ `𝚅𝙴𝚁𝚂𝙸𝙾𝙽:` `{visa_ver}`\n"
+VISA_CAPTION += f"╠•➳➠ `𝙶𝚁𝙾𝚄𝙿:`  [𝙹𝙾𝙸𝙽](t.me/Visa_SUPPORT)\n"
+VISA_CAPTION += f"╠•➳➠ `𝙲𝙷𝙰𝙽𝙽𝙴𝙻:` [𝙹𝙾𝙸𝙽](t.me/Visa_Update)\n"
+VISA_CAPTION += f"╠•➳➠ `𝙲𝚁𝙴𝙰𝚃𝙾𝚁:` [⚡𝙿𝚁𝙾⚡](https://t.me/CALL_ME_VP)\n"
+VISA_CAPTION += f"╚═══════════════╝\n\n"
+VISA_CAPTION += " [✨𝚁𝙴𝙿𝙾✨](https://github.com/callmevp/VisaXBot) 🔹 [📜𝙻𝙸𝙲𝙴𝙽𝚂𝙴📜](https://github.com/callmevp/VisaXBot/blob/main/LICENSE)"
                             
 #_______
 
 
 
-@bot.on(deadly_cmd(outgoing=True, pattern="awake$"))
+@bot.on(visa_cmd(outgoing=True, pattern="awake$"))
 @bot.on(sudo_cmd(pattern="awake$", allow_sudo=True))
-async def up(deadly):
-    if deadly.fwd_from:
+async def up(visa):
+    if visa.fwd_from:
         return
-    await deadly.get_chat()
-    await deadly.delete()
-    await bot.send_file(deadly.chat_id, DEADLY_PIC, caption=deadly_caption)
-    await deadly.delete()
+    await visa.get_chat()
+    await visa.delete()
+    await bot.send_file(visa.chat_id, VISA_PIC, caption=visa_caption)
+    await visa.delete()
 
-deadly_caption = f"🔥 ℓєgєη∂яу αƒ ∂єα∂ℓу кααℓ 🔥\n\n"
-deadly_caption += f"≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\n\n"
-deadly_caption += f"**{Config.ALIVE_MSG}**\n\n"
-deadly_caption += f"≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\n\n"                
-deadly_caption += f"𖣘 𝙰𝙱𝙾𝚄𝚃 𝙼𝚈 𝚂𝚈𝚂𝚃𝙴𝙼 𖣘\n\n"
-deadly_caption += f"➾ `𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽` ➣ `{tel_ver}` \n"
-deadly_caption += f"➾ `𝚂𝚄𝙳𝙾 𝙼𝙾𝙳𝙴:` ➣ `{is_sudo}`\n"
-deadly_caption += f"➾ 𝙼𝚈 𝙲𝙷𝙰𝙽𝙽𝙴𝙻: ➣ [𝙹𝙾𝙸𝙽](t.me/Config.YOUR_CHANNEL)\n"
-deadly_caption += f"➾ 𝙼𝚈 𝙶𝚁𝙾𝚄𝙿: ➣ [𝙹𝙾𝙸𝙽](t.me/Config.YOUR_GROUP)\n\n"
-deadly_caption += f"[✨ 𝙳𝙴𝙿𝙻𝙾𝚈 𝚈𝙾𝚄𝚁 𝙳𝙴𝙰𝙳𝙻𝚈 𝙺𝙰𝙰𝙻 ✨](https://github.com/DEADLY-FIGHTERS/DEADLY-KAAL-BOT)\n" 
+visa_caption = f"🔥 ʟɛɢɛռɖǟʀʏ ǟʄ ʋɨֆǟ-Ӽ ɮօȶ 🔥\n\n"
+visa_caption += f"≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\n\n"
+visa_caption += f"**{Config.ALIVE_MSG}**\n\n"
+visa_caption += f"≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\n\n"                
+visa_caption += f"𖣘 𝙰𝙱𝙾𝚄𝚃 𝙼𝚈 𝚂𝚈𝚂𝚃𝙴𝙼 𖣘\n\n"
+visa_caption += f"➾ `𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽` ➣ `{tel_ver}` \n"
+visa_caption += f"➾ `𝚂𝚄𝙳𝙾 𝙼𝙾𝙳𝙴:` ➣ `{is_sudo}`\n"
+visa_caption += f"➾ 𝙼𝚈 𝙲𝙷𝙰𝙽𝙽𝙴𝙻: ➣ [𝙹𝙾𝙸𝙽](t.me/Config.YOUR_CHANNEL)\n"
+visa_caption += f"➾ 𝙼𝚈 𝙶𝚁𝙾𝚄𝙿: ➣ [𝙹𝙾𝙸𝙽](t.me/Config.YOUR_GROUP)\n\n"
+visa_caption += f"[✨ 𝐃𝐄𝐏𝐋𝐎𝐘 𝐘𝐎𝐔𝐑 𝐕𝐈𝐒𝐀-𝐗 𝐁𝐎𝐓 ✨](https://github.com/callmevp/VisaXBot)\n" 
                                      
                                  
                 
