9576eb20b55b8ebfa2a3f985ba6734a5a48aca16
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-09-09 13:04:13 +0530

    Update google_asst.py

--- VisaXBot/Plugins/google_asst.py
+++ VisaXBot/Plugins/google_asst.py
@@ -9,7 +9,7 @@ from gtts import gTTS
 from . import *
 
 
-@bot.on(deadly_cmd(pattern="trt ?(.*)"))
+@bot.on(visa_cmd(pattern="trt ?(.*)"))
 @bot.on(sudo_cmd(pattern="trt ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
@@ -44,15 +44,15 @@ async def _(event):
     except Exception as exc:
         await edit_or_reply(event, str(exc))
 
-@bot.on(deadly_cmd(pattern=r"trc", outgoing=True))
+@bot.on(visa_cmd(pattern=r"trc", outgoing=True))
 @bot.on(sudo_cmd(pattern=r"trc", allow_sudo=True))
-async def _(deadly):
-    if deadly.fwd_from:
+async def _(visa):
+    if visa.fwd_from:
         return
-    await edit_or_reply(deadly, "**All The Language Codes Can Be Found** \n ⚡ [Here](https://telegra.ph/SfMæisér--𐌷𐌴ࠋࠋ𐌱𐍈𐌸-𐌾𐌰𐍀𐌾-06-04) ⚡", link_preview=False)
+    await edit_or_reply(visa, "**All The Language Codes Can Be Found** \n ⚡ [Here](https://telegra.ph/SfMæisér--𐌷𐌴ࠋࠋ𐌱𐍈𐌸-𐌾𐌰𐍀𐌾-06-04) ⚡", link_preview=False)
 
 
-@bot.on(deadly_cmd(pattern="voice (.*)"))
+@bot.on(visa_cmd(pattern="voice (.*)"))
 @bot.on(sudo_cmd(pattern="voice (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
@@ -119,7 +119,7 @@ async def _(event):
 CmdHelp("google_asst").add_command(
   "voice", "<reply to a msg> <lang code>", "Sends the replied msg content in audio format."
 ).add_command(
-    "trt", "<lang code> <reply to msg>", "Translates the replied message to desired language code. Type '.trc' to get all the language codes", f"trt en - deadlyo | {hl}trt en <reply to msg>"
+    "trt", "<lang code> <reply to msg>", "Translates the replied message to desired language code. Type '.trc' to get all the language codes", f"trt en - visao | {hl}trt en <reply to msg>"
 ).add_command(
   "trc", None, "Gets all the possible language codes for google translate module"
 ).add_info(
